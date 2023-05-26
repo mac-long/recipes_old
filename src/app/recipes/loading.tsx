403ae8062e34e0982585b04e3f0a7b971803eda8
Mac <maclong9@icloud.com>
2023-05-26 21:26:09 +0100

    fix: added loading state

--- src/app/recipes/loading.tsx
+++ src/app/recipes/loading.tsx
@@ -1,5 +1,13 @@
 import RecipeCard from "@/components/recipes/card";
 
 export default function Loading() {
-  return <>{[...Array(10).map((e, i) => <RecipeCard key={i} loading />)]};</>;
+  const cards = [];
+  for (let i = 0; i < 12; i++) cards.push(<RecipeCard key={i} loading />);
+  return (
+    <div className="py-16 bg-white px-6 mx-auto max-w-7xl lg:px-8">
+      <div className="grid grid-cols-1 gap-x-8 pt-10 mx-auto space-y-4 max-w-2xl border-t border-gray-200 sm:pt-16 sm:mt-16 md:gap-y-8 md:space-y-0 lg:grid-cols-3 lg:mx-0 lg:max-w-none">
+        {cards}
+      </div>
+    </div>
+  );
 }
--- src/components/recipes/card.tsx
+++ src/components/recipes/card.tsx
@@ -15,7 +15,7 @@ export default function RecipeCard({
         key={id}
         className="flex flex-col justify-start p-2 max-w-xl rounded-md hover:bg-gray-200"
       >
-        <div className="flex gap-x-4 items-center text-xs lg:items-start">
+        <div className="flex gap-x-4 items-center text-xs">
           <time dateTime={created_on.toDateString()} className="text-gray-500">
             {created_on.toDateString()}
           </time>
@@ -39,15 +39,13 @@ export default function RecipeCard({
 
   return (
     <div className="flex flex-col justify-start p-2 max-w-xl rounded-md bg-gray-400 animate-pulse h-[144px]">
-      <div className="flex gap-x-4 items-center text-xs lg:items-start">
-        <time className="text-gray-500 w-[94px] h-[16px] bg-gray-200 animate-pulse rounded-md" />
+      <div className="flex gap-x-4 items-center text-xs">
+        <time className="text-gray-500 w-[94px] h-[16px] bg-gray-200 animate-pulse rounded-md md:w-[75px] md:h-[32px]" />
         <div className="flex justify-center items-center space-x-1 w-[62px] h-[28px] bg-gray-200 animate-pulse rounded-full" />
       </div>
-      <div className="flex relative flex-col justify-between h-full cursor-pointer group">
-        <h3 className="mt-3 text-lg font-semibold leading-6 group-hover:text-indigo-600 bg-gray-200 w-[200px] h-[30px] text-gray-200 rounded-md animate-pulse">
-          Some placeholder title
-        </h3>
-        <p className="mt-3 text-sm leading-6 text-gray-200 w-[350px] h-[20px] bg-gray-200 rounded-md animate-pulse"></p>
+      <div className="flex relative flex-col justify-between h-full">
+        <h3 className="mt-3 text-lg font-semibold leading-6 bg-gray-200 w-[200px] h-[20px] text-gray-200 rounded-md animate-pulse md:w-[153px] md:h-[25px]" />
+        <p className="text-sm leading-6 text-gray-200 w-[350px] h-[20px] bg-gray-200 rounded-md animate-pulse md:w-[153px] md:h-[45px]" />
       </div>
     </div>
   );
