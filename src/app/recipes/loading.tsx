dd06e4537508464c8fc2d6ba77c3abb7bbd25e38
Mac <maclong9@icloud.com>
2023-05-26 21:56:02 +0100

    fix: loading state title

--- src/app/recipes/loading.tsx
+++ src/app/recipes/loading.tsx
@@ -3,7 +3,7 @@ import RecipeList from "@/components/recipes/recipe-list";
 export default function Loading() {
   return (
     <RecipeList
-      title="Our Recipes."
+      title="Our recipes."
       description="Enjoy searching through our crazy assortment of cuisine from around the world."
       loading
     />
