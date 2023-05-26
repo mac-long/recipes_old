d5c1c0a4b4bad27870a073e63c7e975b070f0edb
Mac <maclong9@icloud.com>
2023-05-26 21:10:33 +0100

    fix: description missing

--- src/components/recipes/recipe-list.tsx
+++ src/components/recipes/recipe-list.tsx
@@ -12,7 +12,9 @@ export default function RecipeList({
 }) {
   return (
     <div className="grid grid-cols-1 gap-x-8 pt-10 mx-auto space-y-4 max-w-2xl border-t border-gray-200 sm:pt-16 sm:mt-16 md:gap-y-8 md:space-y-0 lg:grid-cols-3 lg:mx-0 lg:max-w-none">
-      {title && <RecipesHeading title={title} description={description} />}
+      {title && description && (
+        <RecipesHeading title={title} description={description} />
+      )}
       {recipes.map((recipe) => (
         <RecipeCard key={recipe.id} {...recipe} />
       ))}
