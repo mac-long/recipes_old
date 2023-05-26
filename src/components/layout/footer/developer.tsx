127fe912b54ce5c34168483e7ac89defe85f8e29
Mac <maclong9@icloud.com>
2023-05-26 23:34:49 +0100

    ui: updated hover color of developer

--- src/components/layout/footer/developer.tsx
+++ src/components/layout/footer/developer.tsx
@@ -3,11 +3,17 @@ import Link from "next/link";
 export default function Developer() {
   return (
     <div className="flex absolute bottom-2 justify-center items-center  text-slate-600">
-      <Link href="https://mac-long.vercel.app" target="_blank" rel="norefferer">
+      <Link
+        className="hover:text-indigo-500"
+        href="https://mac-long.vercel.app"
+        target="_blank"
+        rel="norefferer"
+      >
         Developer{" "}
       </Link>
       <span className="mx-2">|</span>
       <Link
+        className="hover:text-indigo-500"
         href="https://github.com/mac-long/recipes"
         target="_blank"
         rel="noreferrer"
