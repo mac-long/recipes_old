41f0253a97b611f44fae9c9e78daf6d054019678
Mac <maclong9@icloud.com>
2023-05-28 05:38:34 +0100

    chore: added storybook plugins

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -5,6 +5,7 @@ const config: StorybookConfig = {
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
     '@storybook/addon-links',
+    '@storybook/addon-a11y'
   ],
   framework: {
     name: '@storybook/nextjs',
